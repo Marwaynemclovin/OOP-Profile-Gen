e27ec0483dd20955b24b143135f0df912fd60316
Marwin Manlangit <marwin.m@hotmail.com>
2023-01-29 15:14:53 +1100

    saving current progress, working on index.js

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require('../lib/Employee');
+
+test('Testing the Name.', () => {
+    const name = 'Melany';
+})
+
+// test('Testing the Email.') () => {
+//     const email = 'melany.mint@gamil.com'
+// }
---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+// Constructors
+const Employee = require('.lib/Employee');
+const Engineer = require('.lib/Engineer');
+const Intern = require('.lib/Inern');
+const Manager = require('.lib/Manager');
